070152eb706d9b927f13f67da4a152dbfe347f6e
Levi M. Smith <lmsmith@coralbyte.tech>
2024-04-18 23:34:44 +0000

    Same origin backend perks

--- frontend/lib/session.ts
+++ frontend/lib/session.ts
@@ -1,32 +1,20 @@
-export const backendUrl: string | undefined =
-  process.env.NEXT_PUBLIC_BACKEND_URL;
-
 async function refreshAccess() {
-  if (backendUrl) {
-    try {
-      const response = await fetch(`${backendUrl}/api/token/refresh/`, {
-        method: "POST",
-        credentials: "include",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-    } catch (error) {
-      console.error("Failed to renew access token: ", error);
-    }
-  } else {
-    console.error("Can't renew token. backendUrl not set.");
+  try {
+    const response = await fetch(`/api/token/refresh/`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.error("Failed to renew access token: ", error);
   }
 }
 
 export async function backendFetch(url: string, options = {}) {
-  if (!backendUrl) {
-    console.error("Cannot fetch. backendUrl is not set");
-    return null;
-  }
-
   // options.credentials = "include";
 
   // Proceed with the original fetch request
-  return fetch(`${backendUrl}${url}`, options);
+  return fetch(`${url}`, options);
 }
