e528d8bcfdf5e7e5a5c4f63f6a825e9f742a8bfa
Levi M. Smith <lmsmith@coralbyte.tech>
2024-05-06 17:38:03 -0600

    Dashboard heatmap base

--- frontend/app/sections/dashboard-client.tsx
+++ frontend/app/sections/dashboard-client.tsx
@@ -32,6 +32,7 @@ import {
   IconDatabase,
 } from "@tabler/icons-react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useContext } from "react";
 
 import {
@@ -64,6 +65,8 @@ export default function Dashboard() {
   const { sharedState, setSharedState } =
     useContext<StateContextType>(StateContext);
 
+  const router = useRouter();
+
   return (
     <Container className="pb-16">
       <h1 className="bigtitle">CMMC Map</h1>
@@ -80,35 +83,76 @@ export default function Dashboard() {
               p="8"
               key={section.section}
               className="sectioncard"
+              onClick={(e) => {
+                router.push(`/sections/${section.section}`);
+              }}
             >
-              <Link
-                href={`/sections/${section.section}`}
-                className="text-inherit no-underline"
-              >
-                <Stack justify="space-between" className="h-full">
-                  <Group wrap="nowrap">
-                    <ActionIcon variant="light">
-                      <Icon />
-                    </ActionIcon>
-                    <Text fw={700}>
-                      {section.section} {section.description} (
-                      {section.abreviation})
-                    </Text>
-                  </Group>
-                  <div className="mb-4">
-                    {sharedState.sectionProgress &&
-                      (isKeyOfSectionProgress(section.section) ? (
-                        <Progress
-                          value={sharedState.sectionProgress[section.section]}
-                        />
-                      ) : (
-                        <Center>
-                          <Loader type="dots" />
-                        </Center>
-                      ))}
-                  </div>
-                </Stack>
-              </Link>
+              <Stack justify="space-between" className="h-full">
+                <Group wrap="nowrap">
+                  <ActionIcon variant="light">
+                    <Icon />
+                  </ActionIcon>
+                  <Text fw={700}>
+                    {section.section} {section.description} (
+                    {section.abreviation})
+                  </Text>
+                </Group>
+                <div className="mb-4">
+                  {sharedState.sectionProgress &&
+                    (isKeyOfSectionProgress(section.section) ? (
+                      controls
+                        .filter((control: Control) => {
+                          const sectionParts = section.section.split(".");
+                          const controlParts = control.section.split(".");
+
+                          return (
+                            sectionParts[0] === controlParts[0] &&
+                            sectionParts[1] === controlParts[1]
+                          );
+                        })
+                        .map((control: Control) => {
+                          const currentControlProgress =
+                            sharedState.controlProgress?.find(
+                              (element: ControlProgress) =>
+                                element.control === control.id,
+                            );
+
+                          let implementation_status = 0;
+
+                          if (currentControlProgress) {
+                            implementation_status =
+                              currentControlProgress.implementation_status;
+                          }
+
+                          return (
+                            <ActionIcon
+                              color={
+                                implementation_status === 1
+                                  ? "green"
+                                  : implementation_status === 2
+                                    ? "yellow"
+                                    : implementation_status === 3
+                                      ? "teal"
+                                      : "gray"
+                              }
+                              m={2}
+                              size="xs"
+                              onClick={(e) => {
+                                e.stopPropagation();
+                                router.push(
+                                  `/sections/${section.section}/${control.section}`,
+                                );
+                              }}
+                            ></ActionIcon>
+                          );
+                        })
+                    ) : (
+                      <Center>
+                        <Loader type="dots" />
+                      </Center>
+                    ))}
+                </div>
+              </Stack>
             </Paper>
           );
         })}
