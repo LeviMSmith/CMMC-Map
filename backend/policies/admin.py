a31dc368e790f5b50db4fdc7b4e20745499a6f0b
LeviMSmith <lsmith@sgaerospace.com>
2024-10-15 10:06:22 -0600

    Cull backend and docker flush

--- backend/policies/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Revision, Assessment
-
-admin.site.register(Revision)
-admin.site.register(Assessment)
--- backend/policies/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class PoliciesConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "policies"
-
-    def ready(self):
-        import policies.signals
